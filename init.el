23364be94b2fa1cc24b240b0552df1637eb9ce4e
Vince West <A3R7LZZ@usac.mmm.com>
2018-03-20 09:53:10 -0500

    updates

--- init.el
+++ init.el
@@ -3,8 +3,7 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  )
+    (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
 (package-initialize)
 
 (custom-set-variables
@@ -18,7 +17,7 @@
     ("3b5ce826b9c9f455b7c4c8bff22c020779383a12f2f57bf2eb25139244bb7290" default)))
  '(package-selected-packages
    (quote
-    (auto-complete company counsel-projectile swiper-helm counsel ivy projectile evil-org yaml-mode use-package markdown-mode htmlize evil ess color-theme)))
+    (company counsel-projectile swiper-helm counsel ivy projectile evil-org yaml-mode use-package markdown-mode htmlize evil ess color-theme)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 
@@ -48,7 +47,15 @@
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(when window-system (set-frame-size (selected-frame) 160 48))
+(add-to-list 'default-frame-alist '(height . 48))
+(add-to-list 'default-frame-alist '(width . 160))
 
+;; I like my background, but this is the only way I can get it without modifying the theme
+(defun vince-background () (interactive)  ;; calling this at the end of the init doesn't work, but calling with M-x does
+    (add-to-list 'default-frame-alist '(background-color . "#F8F8FF"))
+    (set-background-color "#F8F8FF")
+    )
 ;; special function to toggle comments
 (defun comment-or-uncomment-region-or-line ()
     "Comments or uncomments the region or the current line if there's no active region."
@@ -67,7 +74,6 @@
   (require 'use-package))
 (setq use-package-always-ensure t)
 
-
 (use-package evil
     :init
     (evil-mode 1)
@@ -82,6 +88,19 @@
 	org-src-tab-acts-natively t)
     )
 
+(use-package projectile
+    :init
+    (projectile-mode)
+    (counsel-projectile-mode)
+    (setq projectile-completion-system 'ivy)
+    )
+
+(use-package company
+    :init
+    (company-mode)
+    (add-hook 'after-init-hook 'global-company-mode)
+    )
+
 (use-package ivy
     :init
     (ivy-mode 1)
@@ -93,23 +112,26 @@
     (global-set-key (kbd "<f1> l") 'counsel-find-library)
     (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
     (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-    (define-key undo-tree-map (kbd "C-/") 'comment-or-uncomment-region-or-line)
-    )
-
-(use-package projectile
-    :init
-    (projectile-mode)
-    (counsel-projectile-mode)
-    (setq projectile-completion-system 'ivy)
+    (define-key undo-tree-map (kbd "C-/") nil)  ;; so I can use it later for toggling comments
     )
 
 (use-package ess
     :init
-    (setq inferior-R-program-name "C:\\Program Files\\R\\R-3.4.3\\bin\\R.exe")
+    (define-key company-active-map (kbd "M-h") 'company-show-doc-buffer)
+    (define-key company-active-map (kbd "M-n") nil)
+    (define-key company-active-map (kbd "M-p") nil)
+    (define-key company-active-map (kbd "M-,") 'company-select-next)
+    (define-key company-active-map (kbd "M-k") 'company-select-previous)
+    (global-set-key (kbd "<backtab>") 'company-complete-common)
+
+    (setq company-selection-wrap-around t
+	company-tooltip-align-annotations t
+	company-idle-delay 0.2
+	company-minimum-prefix-length 2
+	company-tooltip-limit 25)
     )
 
-;; another comment
 ;; wrap up with changing the default directory
 (setq default-directory "~/")
-(add-to-list 'default-frame-alist '(background-color . "#F8F8FF"))
-(set-background-color "#F8F8FF")
+(global-set-key (kbd "C-/") 'comment-or-uncomment-region-or-line)
+(vince-background)
