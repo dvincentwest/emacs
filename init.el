e730e932fd0cff3138118fd345e947d48d9e579a
Vince <dvincentwest@gmail.com>
2018-10-23 04:39:50 -0500

    adding inconsolata for linux systems

--- init.el
+++ init.el
@@ -37,7 +37,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+ (if (eq system-type 'windows-nt)
+   '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
+   '(default ((t (:family "Inconsolata" :foundry "PfEd" :slant normal :weight normal :height 120 :width normal))))
+   )
+ )
 
 ;; inhibit startup screen
 (setq inhibit-startup-screen t
@@ -184,4 +188,3 @@
   :init
   (elpy-enable)
   )
-
